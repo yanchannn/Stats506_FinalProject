30a5ce53dde5bf575fc3e9c5c0595bc603eccf73
yanchannn <65868499+yanchannn@users.noreply.github.com>
2020-12-14 10:01:16 -0500

    Update data.sh

--- code/data.sh
+++ code/data.sh
@@ -13,8 +13,9 @@
 base_url=https://www.eia.gov/consumption/commercial/data/2012/xls
 
 # download microdata: ---------------------------------------------------------
-
 ## 20012 CBECS
 if [ ! -f 2012_public_use_data_aug2016.csv ]; then
     wget $base_url/2012_public_use_data_aug2016.csv
 fi
+
+# 79: -------------------------------------------------------------------------
